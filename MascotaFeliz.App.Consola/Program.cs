47f281b0ec86d7f0fd468caa5106f9a3f4ee06e4
David Fajardo <79338035+Echendi@users.noreply.github.com>
2022-09-12 11:30:51 -0500

    Revert "Delete MascotaFeliz.App.Consola directory"
    This reverts commit 16b886cce6c5b20cc527ce8917ab9168dbf61abd.

---
+++ MascotaFeliz.App.Consola/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using MascotaFeliz.App.Dominio;
+
+namespace MascotaFeliz.App.Consola
+{
+    public class program
+    {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello world!");
+        }
+    }
+
+}
---
+++ MascotaFeliz.App.Persistencia/Migrations/20220823223746_Inicial.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MascotaFeliz.App.Persistencia.Migrations
+{
+    public partial class Inicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Historias",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Historias", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Personas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombres = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Apellidos = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Direccion = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Telefono = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Discriminator = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Correo = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    TarjetaProfesional = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Personas", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "VisitasPyP",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FechaVisita = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Temperatura = table.Column<float>(type: "real", nullable: false),
+                    Peso = table.Column<float>(type: "real", nullable: false),
+                    FrecuenciaRespiratoria = table.Column<float>(type: "real", nullable: false),
+                    FrecuenciaCardiaca = table.Column<float>(type: "real", nullable: false),
+                    EstadoAnimo = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    IdVeterinario = table.Column<int>(type: "int", nullable: false),
+                    Recomendaciones = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    HistoriaId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_VisitasPyP", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_VisitasPyP_Historias_HistoriaId",
+                        column: x => x.HistoriaId,
+                        principalTable: "Historias",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "mascota",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Color = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Especie = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Raza = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DuenoId = table.Column<int>(type: "int", nullable: true),
+                    VeterinarioId = table.Column<int>(type: "int", nullable: true),
+                    HistoriaId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_mascota", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_mascota_Historias_HistoriaId",
+                        column: x => x.HistoriaId,
+                        principalTable: "Historias",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_mascota_Personas_DuenoId",
+                        column: x => x.DuenoId,
+                        principalTable: "Personas",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_mascota_Personas_VeterinarioId",
+                        column: x => x.VeterinarioId,
+                        principalTable: "Personas",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_mascota_DuenoId",
+                table: "mascota",
+                column: "DuenoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_mascota_HistoriaId",
+                table: "mascota",
+                column: "HistoriaId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_mascota_VeterinarioId",
+                table: "mascota",
+                column: "VeterinarioId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_VisitasPyP_HistoriaId",
+                table: "VisitasPyP",
+                column: "HistoriaId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "mascota");
+
+            migrationBuilder.DropTable(
+                name: "VisitasPyP");
+
+            migrationBuilder.DropTable(
+                name: "Personas");
+
+            migrationBuilder.DropTable(
+                name: "Historias");
+        }
+    }
+}
